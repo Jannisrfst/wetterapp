66a0b26d204b4c2e9e4b8e4b00c828e2e0fc2a37
Jannisrfst <101525736+Jannisrfst@users.noreply.github.com>
2023-09-27 13:59:36 +0200

    Update wetter.py
    Ausgabe: Auf der Seite wenn man register drueckt zwei unterschiedliche wetterdaten lists. Jedoch nach 5 sekunden wieder gleich.
    
    Vermutung: update_weather_data methode aktualisiert die Wetterdaten in class WeatherData fuer alle verbunden clients

--- wetter.py
+++ wetter.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
-from threading import Thread
+from threading import Thread, Event
 import random
 import time
 
@@ -9,8 +9,7 @@ class WeatherData:
         self.humidity = random.randint(0, 100)
         self.pressure = random.randint(950, 1050)
         self.temperature = random.randint(0, 40)
-        self._registered = False
-        self._observers = []
+        self._observers = {}
 
     @staticmethod
     def get_instance():
@@ -21,50 +20,60 @@ class WeatherData:
     def toJson(self):
         return {"humidity": self.humidity, "pressure": self.pressure, "temperature": self.temperature}
 
-    def set_registered(self, registered):
-        self._registered = registered
-
     def add_observer(self, observer):
-        self._observers.append(observer)
+        self._observers[observer.sid] = observer
 
     def notify_observers(self):
-        for observer in self._observers:
+        for observer in self._observers.values():
             observer.on_update(self)
 
+    def update_weather_data(self):
+        while not thread_stop_event.is_set():
+            self.humidity = random.randint(0, 100)
+            self.pressure = random.randint(950, 1050)
+            self.temperature = random.randint(0, 40)
+            self.notify_observers()
+            time.sleep(5)  # update every 5 seconds
+
+    def get_weather_data_for_location(self, latitude, longitude):
+        # Use the latitude and longitude to generate weather data.
+        # This is just a simple example. In a real application, you would
+        # probably want to use a more sophisticated method to generate the weather data.
+        self.humidity = random.randint(0, 100)
+        self.pressure = random.randint(950, 1050)
+        self.temperature = random.randint(0, 40)
+        return self.toJson()
+
 class WeatherObserver:
-    def __init__(self, socketio):
+    def __init__(self, socketio, sid):
         self.socketio = socketio
+        self.sid = sid
 
     def on_update(self, weather_data):
         print("Weather update:", weather_data.toJson())
-        self.socketio.emit("weather_update", weather_data.toJson())
+        self.socketio.emit("weather_update", weather_data.toJson(), room=self.sid)
+
+def background_thread():
+    weather_data.update_weather_data()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 weather_data = WeatherData.get_instance()
-
-observer = WeatherObserver(socketio)
-weather_data.add_observer(observer)
+thread_stop_event = Event()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def generate_weather_data():
-    while True:
-        if weather_data._registered:
-            weather_data.humidity = random.randint(0, 100)
-            weather_data.pressure = random.randint(950, 1050)
-            weather_data.temperature = random.randint(0, 40)
-            weather_data.notify_observers()
-            time.sleep(5)
-
 @socketio.on('location_data')
 def handle_location_data(data):
-    print('Received location data:', data)
-    weather_data.set_registered(True)
-    Thread(target=generate_weather_data).start()
+    observer = WeatherObserver(socketio, request.sid)
+    weather_data.add_observer(observer)
+    weather_data_for_location = weather_data.get_weather_data_for_location(data['latitude'], data['longitude'])
+    emit('weather_update', weather_data_for_location)
 
 if __name__ == '__main__':
+    thread = Thread(target=background_thread)
+    thread.start()
     socketio.run(app)
